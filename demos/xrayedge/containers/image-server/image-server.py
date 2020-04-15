fce9197e7227e17813f5393acc60829d4d3e3222
Guillaume Moutier <guillaume.moutier@gmail.com>
2020-04-14 21:44:49 -0400

    correction

--- demos/xrayedge/containers/image-server/image-server.py
+++ demos/xrayedge/containers/image-server/image-server.py
@@ -25,7 +25,7 @@ def get_last_image(bucket_name):
         cursor = cnx.cursor()
         query = 'SELECT name FROM ' + bucket_table[bucket_name] + ' ORDER BY time DESC LIMIT 1;'
         row_counts = cursor.execute(query)
-        if row_counts > 0:
+        if row_counts:
             data = cursor.fetchone()
             result = data[0]
         else:
